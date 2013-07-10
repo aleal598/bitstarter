c0b1dfd33dd0dd511a7b0d05b79dc80ec006f749
Ubuntu <ubuntu@ip-172-31-4-173.us-west-2.compute.internal>
2013-07-10 18:21:08 +0000

    text

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
+var fs = require('fs'); 
 var express = require('express');
-var fs= required('fs');
 var app = express.createServer(express.logger());
-var data= new Buffer(228);
 
+var data= new Buffer(228);
 data=fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-    response.send(data.toString('utf8'));
+response.send(data.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
